ece1deeb26387c413a1a8cf170e2fdca9b3922a8
Justin Storrer <storrerjustin@gmail.com>
2021-04-07 11:43:10 -0400

    Initial Commit

---
+++ TextView in TableView/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TextView in TableView
+//
+//  Created by Justin Storrer on 4/7/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
